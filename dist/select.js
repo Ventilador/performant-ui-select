6245e4b9ffcb831dd142dfcf2fe575107bb6a998
PPertino <ppertino@hexacta.com>
2017-01-03 16:01:06 -0300

    changing internal ngRepeat to proteusRepeat

--- dist/select.js
+++ dist/select.js
@@ -1,7 +1,7 @@
 /*!
- * ui-select
- * http://github.com/angular-ui/ui-select
- * Version: 0.19.5 - 2016-10-24T23:13:59.434Z
+ * performant-ui-select
+ * https://github.com/Ventilador/performant-ui-select
+ * Version: 0.19.6 - 2017-01-02T20:48:08.918Z
  * License: MIT
  */
 
@@ -118,7 +118,8 @@ var uis = angular.module('ui.select', [])
   },
   appendToBody: false,
   spinnerEnabled: false,
-  spinnerClass: 'glyphicon-refresh ui-select-spin'
+  spinnerClass: 'glyphicon-refresh ui-select-spin',
+  backspaceReset: true
 })
 
 // See Rename minErr and make it accessible from outside https://github.com/angular/angular.js/issues/6913
@@ -179,100 +180,127 @@ var uis = angular.module('ui.select', [])
   };
 }]);
 
-uis.directive('uiSelectChoices',
-  ['uiSelectConfig', 'uisRepeatParser', 'uiSelectMinErr', '$compile', '$window',
-  function(uiSelectConfig, RepeatParser, uiSelectMinErr, $compile, $window) {
+/**
+ * Debounces functions
+ *
+ * Taken from UI Bootstrap $$debounce source code
+ * See https://github.com/angular-ui/bootstrap/blob/master/src/debounce/debounce.js
+ *
+ */
+uis.factory('$$uisDebounce', ['$timeout', function($timeout) {
+  return function(callback, debounceTime) {
+    var timeoutPromise;
 
-  return {
-    restrict: 'EA',
-    require: '^uiSelect',
-    replace: true,
-    transclude: true,
-    templateUrl: function(tElement) {
-      // Needed so the uiSelect can detect the transcluded content
-      tElement.addClass('ui-select-choices');
-
-      // Gets theme attribute from parent (ui-select)
-      var theme = tElement.parent().attr('theme') || uiSelectConfig.theme;
-      return theme + '/choices.tpl.html';
-    },
-
-    compile: function(tElement, tAttrs) {
-
-      if (!tAttrs.repeat) throw uiSelectMinErr('repeat', "Expected 'repeat' expression.");
-
-      // var repeat = RepeatParser.parse(attrs.repeat);
-      var groupByExp = tAttrs.groupBy;
-      var groupFilterExp = tAttrs.groupFilter;
-
-      if (groupByExp) {
-        var groups = tElement.querySelectorAll('.ui-select-choices-group');
-        if (groups.length !== 1) throw uiSelectMinErr('rows', "Expected 1 .ui-select-choices-group but got '{0}'.", groups.length);
-        groups.attr('ng-repeat', RepeatParser.getGroupNgRepeatExpression());
+    return function() {
+      var self = this;
+      var args = Array.prototype.slice.call(arguments);
+      if (timeoutPromise) {
+        $timeout.cancel(timeoutPromise);
       }
 
-      var parserResult = RepeatParser.parse(tAttrs.repeat);
-
-      var choices = tElement.querySelectorAll('.ui-select-choices-row');
-      if (choices.length !== 1) {
-        throw uiSelectMinErr('rows', "Expected 1 .ui-select-choices-row but got '{0}'.", choices.length);
-      }
-
-      choices.attr('ng-repeat', parserResult.repeatExpression(groupByExp))
-             .attr('ng-if', '$select.open'); //Prevent unnecessary watches when dropdown is closed
-    
-
-      var rowsInner = tElement.querySelectorAll('.ui-select-choices-row-inner');
-      if (rowsInner.length !== 1) {
-        throw uiSelectMinErr('rows', "Expected 1 .ui-select-choices-row-inner but got '{0}'.", rowsInner.length);
-      }
-      rowsInner.attr('uis-transclude-append', ''); //Adding uisTranscludeAppend directive to row element after choices element has ngRepeat
-
-      // If IE8 then need to target rowsInner to apply the ng-click attr as choices will not capture the event. 
-      var clickTarget = $window.document.addEventListener ? choices : rowsInner;
-      clickTarget.attr('ng-click', '$select.select(' + parserResult.itemName + ',$select.skipFocusser,$event)');
-      
-      return function link(scope, element, attrs, $select) {
-
-       
-        $select.parseRepeatAttr(attrs.repeat, groupByExp, groupFilterExp); //Result ready at $select.parserResult
-
-        $select.disableChoiceExpression = attrs.uiDisableChoice;
-        $select.onHighlightCallback = attrs.onHighlight;
-
-        $select.dropdownPosition = attrs.position ? attrs.position.toLowerCase() : uiSelectConfig.dropdownPosition;        
-
-        scope.$on('$destroy', function() {
-          choices.remove();
-        });
-
-        scope.$watch('$select.search', function(newValue) {
-          if(newValue && !$select.open && $select.multiple) $select.activate(false, true);
-          $select.activeIndex = $select.tagging.isActivated ? -1 : 0;
-          if (!attrs.minimumInputLength || $select.search.length >= attrs.minimumInputLength) {
-            $select.refresh(attrs.refresh);
-          } else {
-            $select.items = [];
-          }
-        });
-
-        attrs.$observe('refreshDelay', function() {
-          // $eval() is needed otherwise we get a string instead of a number
-          var refreshDelay = scope.$eval(attrs.refreshDelay);
-          $select.refreshDelay = refreshDelay !== undefined ? refreshDelay : uiSelectConfig.refreshDelay;
-        });
-
-        scope.$watch('$select.open', function(open) {
-          if (open) {
-            tElement.attr('role', 'listbox');
-          } else {
-            tElement.removeAttr('role');
-          }
-        });
-      };
-    }
+      timeoutPromise = $timeout(function() {
+        callback.apply(self, args);
+      }, debounceTime);
+    };
   };
 }]);
+
+uis.directive('uiSelectChoices',
+  ['uiSelectConfig', 'uisRepeatParser', 'uiSelectMinErr', '$compile', '$window',
+    function (uiSelectConfig, RepeatParser, uiSelectMinErr, $compile, $window) {
+
+      return {
+        restrict: 'EA',
+        require: '^uiSelect',
+        replace: true,
+        transclude: true,
+        templateUrl: function (tElement) {
+          // Needed so the uiSelect can detect the transcluded content
+          tElement.addClass('ui-select-choices');
+
+          // Gets theme attribute from parent (ui-select)
+          var theme = tElement.parent().attr('theme') || uiSelectConfig.theme;
+          return theme + '/choices.tpl.html';
+        },
+
+        compile: function (tElement, tAttrs) {
+
+          if (!tAttrs.repeat) throw uiSelectMinErr('repeat', "Expected 'repeat' expression.");
+
+          // var repeat = RepeatParser.parse(attrs.repeat);
+          var groupByExp = tAttrs.groupBy;
+          var groupFilterExp = tAttrs.groupFilter;
+
+          if (groupByExp) {
+            var groups = tElement.querySelectorAll('.ui-select-choices-group');
+            if (groups.length !== 1) throw uiSelectMinErr('rows', "Expected 1 .ui-select-choices-group but got '{0}'.", groups.length);
+            groups.attr('ng-repeat', RepeatParser.getGroupNgRepeatExpression());
+          }
+
+          var parserResult = RepeatParser.parse(tAttrs.repeat);
+
+          var choices = tElement.querySelectorAll('.ui-select-choices-row');
+          if (choices.length !== 1) {
+            throw uiSelectMinErr('rows', "Expected 1 .ui-select-choices-row but got '{0}'.", choices.length);
+          }
+
+          choices.wrap(
+            angular.element('<div/>')
+              .attr('proteus-repeat', parserResult.repeatExpression(groupByExp))
+              .attr('ng-if', '$select.open'));
+
+
+          // choices.attr('proteus-repeat', parserResult.repeatExpression(groupByExp))
+          //        .attr('ng-if', '$select.open'); //Prevent unnecessary watches when dropdown is closed
+
+
+          var rowsInner = tElement.querySelectorAll('.ui-select-choices-row-inner');
+          if (rowsInner.length !== 1) {
+            throw uiSelectMinErr('rows', "Expected 1 .ui-select-choices-row-inner but got '{0}'.", rowsInner.length);
+          }
+          //  rowsInner.attr('uis-transclude-append', ''); //Adding uisTranscludeAppend directive to row element after choices element has ngRepeat
+
+          // If IE8 then need to target rowsInner to apply the ng-click attr as choices will not capture the event. 
+          var clickTarget = $window.document.addEventListener ? choices : rowsInner;
+          clickTarget.attr('ng-click', '$select.select(' + parserResult.itemName + ',$select.skipFocusser,$event)');
+
+          return function link(scope, element, attrs, $select) {
+
+
+            $select.parseRepeatAttr(attrs.repeat, groupByExp, groupFilterExp); //Result ready at $select.parserResult
+
+            $select.disableChoiceExpression = attrs.uiDisableChoice;
+            $select.onHighlightCallback = attrs.onHighlight;
+
+            $select.dropdownPosition = attrs.position ? attrs.position.toLowerCase() : uiSelectConfig.dropdownPosition;
+
+            scope.$watch('$select.search', function (newValue) {
+              if (newValue && !$select.open && $select.multiple) $select.activate(false, true);
+              $select.activeIndex = $select.tagging.isActivated ? -1 : 0;
+              if (!attrs.minimumInputLength || $select.search.length >= attrs.minimumInputLength) {
+                $select.refresh(attrs.refresh);
+              } else {
+                $select.items = [];
+              }
+            });
+
+            attrs.$observe('refreshDelay', function () {
+              // $eval() is needed otherwise we get a string instead of a number
+              var refreshDelay = scope.$eval(attrs.refreshDelay);
+              $select.refreshDelay = refreshDelay !== undefined ? refreshDelay : uiSelectConfig.refreshDelay;
+            });
+
+            scope.$watch('$select.open', function (open) {
+              if (open) {
+                element.attr('role', 'listbox');
+              } else {
+                element.removeAttr('role');
+              }
+            });
+          };
+        }
+      };
+    }]);
 
 /**
  * Contains ui-select "intelligence".
@@ -297,7 +325,6 @@ uis.controller('uiSelectCtrl',
   ctrl.refreshing = false;
   ctrl.spinnerEnabled = uiSelectConfig.spinnerEnabled;
   ctrl.spinnerClass = uiSelectConfig.spinnerClass;
-
   ctrl.removeSelected = uiSelectConfig.removeSelected; //If selected item(s) should be removed from dropdown list
   ctrl.closeOnSelect = true; //Initialized inside uiSelect directive link function
   ctrl.skipFocusser = false; //Set to true to avoid returning focus to ctrl when item is selected
@@ -581,7 +608,7 @@ uis.controller('uiSelectCtrl',
         var refreshPromise =  $scope.$eval(refreshAttr);
         if (refreshPromise && angular.isFunction(refreshPromise.then) && !ctrl.refreshing) {
           ctrl.refreshing = true;
-          refreshPromise.then(function() {
+          refreshPromise.finally(function() {
             ctrl.refreshing = false;
           });
       }}, ctrl.refreshDelay);
@@ -706,20 +733,10 @@ uis.controller('uiSelectCtrl',
             ctrl.close(skipFocusser);
             return;
           }
-        }        
+        }
         _resetSearchInput();
         $scope.$broadcast('uis:select', item);
-
-        var locals = {};
-        locals[ctrl.parserResult.itemName] = item;
-
-        $timeout(function(){
-          ctrl.onSelectCallback($scope, {
-            $item: item,
-            $model: ctrl.parserResult.modelMapper($scope, locals)
-          });
-        });
-
+        
         if (ctrl.closeOnSelect) {
           ctrl.close(skipFocusser);
         }
@@ -782,7 +799,7 @@ uis.controller('uiSelectCtrl',
         }
 
       if (!isLocked && lockedItemIndex > -1) {
-        lockedItems.splice(lockedItemIndex, 0);
+        lockedItems.splice(lockedItemIndex, 1);
       }
     }
 
@@ -1115,6 +1132,12 @@ uis.directive('uiSelect',
         scope.$watch('sortable', function() {
             var sortable = scope.$eval(attrs.sortable);
             $select.sortable = sortable !== undefined ? sortable : uiSelectConfig.sortable;
+        });
+
+        attrs.$observe('backspaceReset', function() {
+          // $eval() is needed otherwise we get a string instead of a boolean
+          var backspaceReset = scope.$eval(attrs.backspaceReset);
+          $select.backspaceReset = backspaceReset !== undefined ? backspaceReset : true;
         });
 
         attrs.$observe('limit', function() {
@@ -1679,6 +1702,15 @@ uis.directive('uiSelectMultiple', ['uiSelectMinErr','$timeout', function(uiSelec
           return;
         }
         $select.selected.push(item);
+        var locals = {};        
+        locals[$select.parserResult.itemName] = item;
+
+        $timeout(function(){
+          $select.onSelectCallback(scope, {
+            $item: item,
+            $model: $select.parserResult.modelMapper(scope, locals)
+          });
+        });
         $selectMultiple.updateModel();
       });
 
@@ -2022,6 +2054,15 @@ uis.directive('uiSelectSingle', ['$timeout','$compile', function($timeout, $comp
 
       scope.$on('uis:select', function (event, item) {
         $select.selected = item;
+        var locals = {};        
+        locals[$select.parserResult.itemName] = item;
+
+        $timeout(function(){
+          $select.onSelectCallback(scope, {
+            $item: item,
+            $model: $select.parserResult.modelMapper(scope, locals)
+          });
+        });
       });
 
       scope.$on('uis:close', function (event, skipFocusser) {
@@ -2056,7 +2097,7 @@ uis.directive('uiSelectSingle', ['$timeout','$compile', function($timeout, $comp
       });
       focusser.bind("keydown", function(e){
 
-        if (e.which === KEY.BACKSPACE) {
+        if (e.which === KEY.BACKSPACE && $select.backspaceReset !== false) {
           e.preventDefault();
           e.stopPropagation();
           $select.select(undefined);
@@ -2240,31 +2281,6 @@ uis.directive('uiSelectSort', ['$timeout', 'uiSelectConfig', 'uiSelectMinErr', f
         element.off('drop', dropHandler);
       });
     }
-  };
-}]);
-
-/**
- * Debounces functions
- *
- * Taken from UI Bootstrap $$debounce source code
- * See https://github.com/angular-ui/bootstrap/blob/master/src/debounce/debounce.js
- *
- */
-uis.factory('$$uisDebounce', ['$timeout', function($timeout) {
-  return function(callback, debounceTime) {
-    var timeoutPromise;
-
-    return function() {
-      var self = this;
-      var args = Array.prototype.slice.call(arguments);
-      if (timeoutPromise) {
-        $timeout.cancel(timeoutPromise);
-      }
-
-      timeoutPromise = $timeout(function() {
-        callback.apply(self, args);
-      }, debounceTime);
-    };
   };
 }]);
 
